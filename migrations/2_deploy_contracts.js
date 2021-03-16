eea952fa55bee44828166478f410a40acb225b55
Kevin Kamto <kamtokevin@gmail.com>
2021-03-15 21:48:45 -0400

    chore: deps + deployment

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,4 @@
+const { Web3Provider } = require("ethers/providers");
+
+const AEToken = artifacts.require('../contracts/AEToken/AEToken.sol');
+const Governance = artifacts.require('../contracts/Governance.sol');
