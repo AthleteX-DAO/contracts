a723e8df115ab1bd2e3db42e0a7811306f1f7d42
Kevin Kamto <kamtokevin@gmail.com>
2021-03-26 13:09:45 -0400

    feat: added athlete type

--- contracts/AthleteFactory/AthleteFactory.sol
+++ contracts/AthleteFactory/AthleteFactory.sol
@@ -1,9 +1,16 @@
 pragma solidity ^0.7.1;
 import './CloneFactory';
 
+// Athletes are functionally the same, just differentiated by price / feed
+event newAthlete(uint aID, string name, uint aPrice );
 
 contract AthleteFactory is CloneFactory 
 {
+    struct Athletes {
+        string aName;
+        uint aPrice;
+    }
+
     Athletes[] public Athlete;
     address masterContract;
 
@@ -22,4 +29,6 @@ contract Athlete {
     function init(uint _price) external {
         price = _price;
     }
+
+    // Need to create oracle
 }
