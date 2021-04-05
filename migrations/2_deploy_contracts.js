f9a8e214300f9839238dc84d3f22a505307e7a50
Kevin Kamto <kamtokevin@gmail.com>
2021-04-04 23:34:59 -0400

    misc:truffle changes

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,3 +2,10 @@ const { Web3Provider } = require("ethers/providers");
 
 const AEToken = artifacts.require('../contracts/AEToken/AEToken.sol');
 const Price = artifacts.require('../contracts/Price.sol');
+const AEStaking = artifacts.require('../contracts/AEToken/AEStaking.sol');
+// const AthleteFactory = artifacts.require('../contracts/AthleteFactory/AthleteFactory.sol');
+
+module.exports = function(deployer) {
+    deployer.deploy(AEToken);
+    deployer.deploy(AEStaking);
+}
