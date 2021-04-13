19b1a69818dccbac26f8cc783cefde2094c9dfb9
Kevin Kamto <kamtokevin@gmail.com>
2021-04-12 20:46:15 -0400

    chore: delete fluff

--- migrations/staking_contracts.js
+++
@@ -1,7 +0,0 @@
-const Staking = artifacts.require("../contracts/AEToken/AEStaking.sol");
-
-
-module.exports = function (deployer) {
-    deployer.deploy(Migrations);
-  };
-  
--- migrations/token_contracts.js
+++
@@ -1,8 +0,0 @@
-const { Web3Provider } = require("ethers/providers");
-
-const AEToken = artifacts.require('../contracts/AEToken/AEToken.sol');
-const Price = artifacts.require('../contracts/Price.sol');
-
-module.exports = function (deployer) {
-    deployer.deploy(Migrations);
-  };  
