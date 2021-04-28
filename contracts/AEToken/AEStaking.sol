75af7673761a7a9c878d1b8f685d54a7d37fcd9d
Kevin Kamto <kamtokevin@gmail.com>
2021-04-28 08:34:36 -0400

    refactor: aestaking rewrite

--- contracts/AEToken/AEStaking.sol
+++ contracts/AEToken/AEStaking.sol
@@ -4,9 +4,12 @@ pragma solidity >=0.6.0 <0.8.0;
 import 'synthetix/contracts/interfaces/IStakingRewards.sol';
 
 contract AEStaking {
+
+
     address public contractAddress;
     address public stakerAddress;
     uint256 private stakedAmount;
+    
 
     IStakingRewards stakingContract;
     event SetNewStakerAddress();
@@ -23,16 +26,34 @@ contract AEStaking {
         emit SetNewStakerAddress(contractAddress);
     }
 
-    function deposit(uint256 _stakedAmount)
-    {
-        // enter the staking contract
-        stakingContract.stake(_stakedAmount);
+    function stake(uint256 amount) external {
+        require(amount > 0, "Cannot stake 0");
+        _totalSupply = _totalSupply.add(amount);
+        _balances[msg.sender] = _balances[msg.sender].add(amount);
+        stakingToken.safeTransferFrom(msg.sender, address(this), amount);
+        emit Staked(msg.sender, amount);
     }
-    
-    function withdraw(uint256 _withdrawAmount);
-    {
-        require(_withdrawAmount < stakedAmount);
-        
+
+    function withdraw(uint256 amount) public {
+        require(amount > 0, "Cannot withdraw 0");
+        _totalSupply = _totalSupply.sub(amount);
+        _balances[msg.sender] = _balances[msg.sender].sub(amount);
+        stakingToken.safeTransfer(msg.sender, amount);
+        emit Withdrawn(msg.sender, amount);
+    }
+
+    function getReward() public nonReentrant updateReward(msg.sender) {
+        uint256 reward = rewards[msg.sender];
+        if (reward > 0) {
+            rewards[msg.sender] = 0;
+            rewardsToken.safeTransfer(msg.sender, reward);
+            emit RewardPaid(msg.sender, reward);
+        }
+    }
+
+    function exit() external {
+        withdraw(_balances[msg.sender]);
+        getReward();
     }
 
 }
