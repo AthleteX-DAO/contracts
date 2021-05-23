b96ce91c918f13ab51ccf831c6db8e44015f9907
Kevin Kamto <kamtokevin@gmail.com>
2021-05-23 13:19:15 -0400

    cleanup: unnecessary import

--- migrations/2_staking_contracts.js
+++ migrations/2_staking_contracts.js
@@ -1,5 +1,4 @@
 const Staking = artifacts.require('../contracts/Staking');
-const BN = require('bn.js');
 module.exports = function (deployer, network, accounts) {
     deployer.deploy(Staking);
   };
