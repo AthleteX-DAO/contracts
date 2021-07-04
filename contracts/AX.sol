fc81508b562797c35214999442238a5e83981e52
Kevin Kamto <kamtokevin@gmail.com>
2021-07-04 03:06:46 -0400

    fix: removed snapshot options

--- contracts/AX.sol
+++ contracts/AX.sol
@@ -1,29 +1,16 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.7.0;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Snapshot.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract AthleteX is ERC20, ERC20Burnable, ERC20Snapshot, Ownable {
+contract AthleteX is ERC20, ERC20Burnable, Ownable {
     constructor() ERC20("AthleteX", "AX") {
         _mint(msg.sender, 10 * 10 ** decimals());
     }
 
-    function snapshot() public onlyOwner {
-        _snapshot();
-    }
-
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
-    }
-
-    // Take a snapshot
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
-        internal
-        override(ERC20, ERC20Snapshot)
-    {
-        super._beforeTokenTransfer(from, to, amount);
     }
 }
