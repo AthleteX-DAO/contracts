d2cf69dd151086ef454f6eb18a5551b5b8ef7723
Kevin Kamto <kamtokevin@gmail.com>
2021-08-06 15:05:47 -0400

    chore: misc changes

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,4 +1,7 @@
 const Migrations = artifacts.require("../contracts/Migrations");
+const StakingRewards = artifacts.require('../contracts/StakingRewards');
+
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+  // deployer.deploy(Migrations);
+  deployer.deploy(StakingRewards);
 };
--- migrations/2_staking_contracts.js
+++ migrations/2_staking_contracts.js
@@ -1,5 +1,1 @@
-const Staking = artifacts.require('../contracts/Staking');
-module.exports = function (deployer, network, accounts) {
-    deployer.deploy(Staking);
-  };
   
