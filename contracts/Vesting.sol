571a13f24fb262010cec355d1a25ff4a9ca91222
Kevin Kamto <kamtokevin@gmail.com>
2021-10-01 21:49:32 -0400

    feat: completed vesting contract

--- contracts/Vesting.sol
+++ contracts/Vesting.sol
@@ -1,24 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./libraries/SafeERC20.sol";
-import "./interfaces/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Vesting is Ownable {
     using SafeERC20 for IERC20;
 
-    uint256 public start = 1632974400; // 9/30/2021 12 AM UTC
-    uint256 public end = 1790740800; // 9/30/2026 12 AM UTC
+    // Starts now and ends in 60 seconds
+    uint256 public start = 1633140000;  // Start Date: 10/1/2021 22:00 EST
+    uint256 public end = 1759370400;   // End Date: 10/1/2025 22:00 EST
 
-    // TODO - update IERC20($AX-ADDRESS)
-    IERC20 public constant AX = IERC20(0x403A39b1161c87eF709a37B0CE78163700BBaa89);
+    IERC20 public constant AX = IERC20(0x5617604BA0a30E0ff1d2163aB94E50d8b6D0B0Df);
     mapping(address => uint256) private _vested;
     mapping(address => uint256) private _total;
 
     constructor() {
-        // TODO - replace default address with team wallet addresses & update ETHER count
-        _total[0x82BBd2F08a59f5be1B4e719ff701e4D234c4F8db] = 1018750 ether;  // Aydan
+        _total[0xe8a59f004D2A14B3c6Fd2dF93c1B609DdbDA9813] = 6698059 ether; // Kevin - 1 token allocated
+        _total[0xa93e39E1f2C87b407E723Fe9135e3571E1a44720] = 2184867 ether; // Alex - 1 token allocated
+        _total[0x3266f39E8B1F6DeC962C3903AeFEBdA602c4b2b1] = 2031378 ether; // Aydan - 1 token allocated
+        _total[0x36A9941835335Aa6dbE69AF6bBFaB778c0992f87] = 6698059 ether; // Robbie - 1 token allocated
     }
 
     function vest() external {
