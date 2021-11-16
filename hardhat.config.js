856e2130d396446c35a13d398101b6415a0d60f3
Kevin Kamto <kamtokevin@gmail.com>
2021-11-16 00:01:52 -0500

    hardhat setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,8 +2,30 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 
- require("@nomiclabs/hardhat-etherscan");
+  require("@nomiclabs/hardhat-etherscan");
+  require("@nomiclabs/hardhat-waffle");
+  require("hardhat-gas-reporter");
+  require("solidity-coverage");
+
+  var $mnem = "off neither whip umbrella skill monitor wall cup style fatal device month";
 
 module.exports = {
-  solidity: "0.7.3",
+  defaultNetwork: "matic",
+  solidity: "0.8.0",
+
+  networks: {
+    matic: { // MATIC MAINNET
+      url: "https://polygon-rpc.com/",
+      accounts: []
+    },
+    mumbai: { //MATIC TESTNET
+      url: "https://matic-mumbai.chainstacklabs.com",
+      accounts: {
+        mnemonic: $mnem,
+      }
+    },
+  },
+  mocha: {
+    timeout: 20000
+  }
 };
